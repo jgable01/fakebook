6b0be6ec638a9e2e2cb6144c5e0fa7547b6e815b
Josh <josh@gable.tech>
2023-03-21 10:09:19 -0500

    Project base

---
+++ README.md
@@ -0,0 +1,3 @@
+Fakebook page. Functionality for users to post both text and images.
+<br>
+Click [here](https://link-url-here.org) to view a demo
---
+++ assets/script/script.js
@@ -0,0 +1,18 @@
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+  Object-oriented JavaScript
+  Joshua Gable
+
+  Assignment 3
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+'use strict'
+
+
+
+
+
+
+
+
+
